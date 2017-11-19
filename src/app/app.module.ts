240c579f35fa442fffd17a845bd547197f65c6f2
inkemi <MiiRuska2905>
2017-11-19 23:33:25 +0200

    versio 0.0.0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { ContactsComponent } from './contact/contacts/contacts.component';
+import { AddContactComponent } from './contact/add-contact/add-contact.component';
+import {FormsModule} from '@angular/forms';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactsComponent,
+    AddContactComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
