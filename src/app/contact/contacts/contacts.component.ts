85ff2644905acd5442e765a3f4f7641ea4ca3257
inkemi <MiiRuska2905>
2017-11-19 23:43:05 +0200

    versio 0.0.1

---
+++ src/app/contact/contacts/contacts.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contacts',
+  templateUrl: './contacts.component.html',
+  styleUrls: ['./contacts.component.scss']
+})
+export class ContactsComponent implements OnInit {
+
+  contactCount: number;
+  firstName: string;
+  lastName: string;
+  phone: string;
+  street: string;
+  city: string;
+  btnText = 'Add a Contact';
+  contacts = [];
+  contact: string;
+
+
+  constructor() { }
+
+  ngOnInit() {
+    this.contactCount = this.contacts.length;
+  }
+  addContact() {
+    this.contact = this.firstName + ' ' + this.lastName + ' Phone: ' + this.phone + ' Address: ' + this.street + ' ' +this.city;
+    this.contacts.push(this.contact);
+    this.contact = '';
+    this.contactCount = this.contacts.length;
+  }
+
+}
